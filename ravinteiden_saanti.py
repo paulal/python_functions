b12032833091be4f26d46ce097be4ce33b80cd55
paulal <paula.laesvuori@gmail.com>
2020-11-02 14:55:52 +0200

    unittest for ravinteiden_saanti.py added

--- ravinteiden_saanti.py
+++ ravinteiden_saanti.py
@@ -14,6 +14,7 @@ saantisuositus_2014.csv - päivittäiset saantisuositukset
 
 '''
 import pandas as pd
+from scipy.optimize import linprog
 
 # pois laskuista jätettävät elintarviketyypit (FUCLASS):
 # lastenruoat, äidinmaidonkorvikkeet, ateriankorvikkeet ja lisäravinteet
@@ -64,11 +65,54 @@ def read_files(path:str) -> tuple:
     food = pd.read_csv(path + "food_utf-8.csv", sep=";")
     saantisuositus = pd.read_csv(path + "saantisuositus_2014.csv", sep=";", header=None, names=["EUFDNAME", "name", "mnuori", "maikuinen", "mkeski", "miäkäs", "mvanha", "npieni","nnuori", "naikuinen", "nkeski", "niäkäs", "nvanha"])
 
-    # delete unwanted data
-    for food_type in omitted_food_types:
-        food = food[food.FUCLASS != food_type]
-    
     return component_value, eufdname, food, saantisuositus
 
 component_value, eufdname, food, saantisuositus = read_files('/home/pomo/Asiakirjat/Kurssit/Taitotalo_Python-ohjelmoija/python/portfolio/fineli_20/')
-print(saantisuositus)
+
+# ateriankorvikkeiden yms. poisto
+
+def filter_food_class(dataframe:pd.DataFrame, class_to_remove:tuple, col:str='FUCLASS') -> pd.DataFrame:
+    """Take a pandas dataframe with food data and remove
+    lines where the FUCLASS is one of those specified in 
+    fuclass_to_remove. Returns the cleaned dataframe.
+
+    Args:
+        dataframe (pandas_df): A pandas dataframe with food names and food classes (e.g. FUCLASS)
+        class_to_remove (tuple): A tuple of food class names to use for filtering out unwanted data
+        col (str): Column name, default = FUCLASS
+
+    Returns:
+        pandas_df: The pandas dataframe minus the lines with specified food class names
+    """
+    if col == 'FUCLASS':
+        for food_class in class_to_remove:
+            dataframe = dataframe[dataframe.FUCLASS != food_class]
+    elif col == 'IGCLASS':
+        for food_class in class_to_remove:
+            dataframe = dataframe[dataframe.IGCLASS != food_class]
+    else:
+        raise ValueError(f'Invalid column name: {col}')
+
+    return dataframe
+
+# puuttuvien ravintoarvotietojen käsittely
+
+def transpose_component_value(dataframe:pd.DataFrame) -> pd.DataFrame:
+    """Takes a pandas dataframe (component_value) where each row represents
+    the amount of one nutrient in one food (e.g. calcium in milk). 
+    Transposes the dataframe so that there is one row per food and one column per nutrient.
+    Fills in any missing component values with zeroes in the appropriate column.
+    The ACQTYPE, METHTYPE, and METHIND columns are dropped.
+
+    Args:
+        dataframe (pandas_df): A pandas dataframe with information on how much of each nutrient foods contain
+
+    Returns:
+        pandas_df: The pandas dataframe modified so that it has one row per food and one column per nutrient.
+    """
+    df = dataframe.drop(columns=['ACQTYPE','METHTYPE', 'METHIND'])
+
+    new_df = df.pivot_table(values='BESTLOC', index='FOODID', columns='EUFDNAME', fill_value=0.0)
+    
+    return new_df
+
---
+++ test_ravinteiden_saanti.py
@@ -0,0 +1,62 @@
+import unittest
+import pandas as pd
+# pandas-dataframen testaus ongelma, self.assertEqual ei suoraan toimi;
+# käytän pandasin omaa testiä pdt.assert_frame_equal
+import pandas.util.testing as pdt
+
+from ravinteiden_saanti import filter_food_class, transpose_component_value
+
+class NutrientTest(unittest.TestCase):
+
+    def test_filter_food_class(self):
+        original_data = {'FOODNAME': ['SOKERI', 'SUKLAA, TUMMA', 'KURKKU',
+        'NAUDANLIHA-KASVISSOSE, TEOLLINEN LASTENRUOKA, TUOTEKESKIARVO',
+        'ATERIANKORVIKE, SHAKE, VALMISJUOMA, NATURDIET', 'POROKEITTO'], 
+        'IGCLASS': ['SUGARSYR', 'CHOCOLAT', 'VEGFRU', 'DISH', 'MEALREP', 'DISH'], 
+        'FUCLASS': ['SUGADD', 'CHOCOL', 'VEGFRESH', 'BABMEATD', 'MEALREP', 'MEATSOUP']}
+        original_df = pd.DataFrame(original_data, index=[1, 32, 346, 11472, 32619, 33479])
+
+        filtered_data1 = {'FOODNAME': ['SOKERI', 'SUKLAA, TUMMA', 'KURKKU',
+        'POROKEITTO'], 
+        'IGCLASS': ['SUGARSYR', 'CHOCOLAT', 'VEGFRU', 'DISH'], 
+        'FUCLASS': ['SUGADD', 'CHOCOL', 'VEGFRESH', 'MEATSOUP']}
+        filtered_df1 = pd.DataFrame(filtered_data1, index=[1, 32, 346, 33479])
+
+        filtered_data2 = {'FOODNAME': ['KURKKU',
+        'NAUDANLIHA-KASVISSOSE, TEOLLINEN LASTENRUOKA, TUOTEKESKIARVO',
+        'ATERIANKORVIKE, SHAKE, VALMISJUOMA, NATURDIET', 'POROKEITTO'], 
+        'IGCLASS': ['VEGFRU', 'DISH', 'MEALREP', 'DISH'], 
+        'FUCLASS': ['VEGFRESH', 'BABMEATD', 'MEALREP', 'MEATSOUP']}
+        filtered_df2 = pd.DataFrame(filtered_data2, index=[346, 11472, 32619, 33479])
+
+        pdt.assert_frame_equal(filter_food_class(original_df, []), original_df)
+        pdt.assert_frame_equal(filter_food_class(original_df, ['BABMEATD', 'MEALREP'], 'FUCLASS'), filtered_df1)
+        pdt.assert_frame_equal(filter_food_class(original_df, ['BABMEATD', 'MEALREP']), filtered_df1)
+        pdt.assert_frame_equal(filter_food_class(original_df, ['SUGARSYR', 'CHOCOLAT'], 'IGCLASS'), filtered_df2)
+        self.assertRaises(ValueError, filter_food_class, original_df, ["BABMEATD"], 'EUFDNAME')
+
+    def test_transpose_component_value(self):
+        original_data = {'FOODID': [1,1,1,1, 2, 2, 2, 2], 
+        'EUFDNAME': ['ENERC', 'FAT', 'CHOAVL', 'CHOCDF', 'ENERC', 'FAT', 'CHOAVL', 'CHOCDF'], 
+        'BESTLOC': [1698.30, None, 99.90, 99.88, 1590.0, 1.6, None, 99.0],
+        'ACQTYPE': ['S', 'F', 'S', 'S', 'S', 'F', 'S', 'S'], 
+        'METHTYPE': ['S', 'E', 'S', 'S', 'S', 'E', 'S', 'S'],
+        'METHIND': ['MI0107', 'MI0107', 'MI0107', 'MI0107', 'MI0107', 'MI0107', 'MI0107', 'MI0107']}
+        original_df = pd.DataFrame(original_data)
+        print(f'original df:', original_df, sep='\n')
+
+        transposed_data = {'FOODID': [1, 2],
+        'CHOAVL': [99.90, 0], 'CHOCDF': [99.88, 99.0], 'ENERC': [1698.30, 1590.0],
+        'FAT': [0.0, 1.6]}
+        transposed_df = pd.DataFrame(transposed_data)
+        print(f'transposed df:', transposed_df, sep='\n')
+        transposed_df.set_index('FOODID', inplace=True)
+        print(f'transposed df:', transposed_df, sep='\n')
+
+        print('returned: ', transpose_component_value(original_df), sep='\n')
+
+        pdt.assert_frame_equal(transpose_component_value(original_df), transposed_df, check_names=False)
+
+
+if __name__ == '__main__':
+    unittest.main()
